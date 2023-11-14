6bc6354970e269a4093ab8ec4eaf50bf2d0d35b8
sobatnawar <121812713+sobatnawar@users.noreply.github.com>
2023-11-14 21:20:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
+	header('Location: '.$uri.'/home/');
 	exit;
 ?>
-Something is wrong with the XAMPP installation :-(
