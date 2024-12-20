901fede5cc26eede285ab6145d5089f14dc39eee
sobatnawar <121812713+sobatnawar@users.noreply.github.com>
2024-12-20 21:08:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('regForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pendaftaran berhasil! Terima kasih telah mendaftar.');
+    // Di sini Anda bisa menambahkan logika untuk mengirim data ke server
+});
